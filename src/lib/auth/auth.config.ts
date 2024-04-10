def2e29b43367c81db50a79729f163e89298235b
Sandip Pathak <pathaksandip321@gmail.com>
2024-04-10 17:50:32 +0545

    dash

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -41,47 +41,47 @@ export async function getUser(email: string): Promise<User | null> {
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: {
-          label: "Username:",
-          type: "text",
-        },
-        password: {
-          label: "Password:",
-          type: "password",
-        },
-      },
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
+    // CredentialsProvider({
+    //   name: "credentials",
+    //   credentials: {
+    //     username: {
+    //       label: "Username:",
+    //       type: "text",
+    //     },
+    //     password: {
+    //       label: "Password:",
+    //       type: "password",
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     const parsedCredentials = z
+    //       .object({ email: z.string().email(), password: z.string().min(6) })
+    //       .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
+    //     if (parsedCredentials.success) {
+    //       const { email, password } = parsedCredentials.data;
 
-          const user = await getUser(email);
-          console.log(user);
-          if (!user) {
-            throw new Error("User not found. Please enter valid credentials");
-          }
+    //       const user = await getUser(email);
+    //       console.log(user);
+    //       if (!user) {
+    //         throw new Error("User not found. Please enter valid credentials");
+    //       }
 
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) {
-            return {
-              ...user,
+    //       const passwordsMatch = await bcrypt.compare(password, user.password);
+    //       if (passwordsMatch) {
+    //         return {
+    //           ...user,
 
-              email: user.email,
-            };
-          } else {
-            throw new Error("Password is incorrect");
-          }
-        }
+    //           email: user.email,
+    //         };
+    //       } else {
+    //         throw new Error("Password is incorrect");
+    //       }
+    //     }
 
-        return null;
-      },
-    }),
+    //     return null;
+    //   },
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -95,14 +95,6 @@ export const authOptions: NextAuthOptions = {
     signIn: "/",
   },
   callbacks: {
-    async signIn({ account, profile }) {
-      if (account && account.provider === "google") {
-        console.log(profile?.email);
-
-        // return profile && profile.email_verified && profile.email.endsWith("@example.com")
-      }
-      return true; // Do different verification for other providers that don't have `email_verified`
-    },
     async jwt({ token, user }) {
       if (user) {
         token.email = user.email;
@@ -116,6 +108,14 @@ export const authOptions: NextAuthOptions = {
       }
       console.log("gs", session);
       return session;
+    },
+    async signIn({ account, profile }) {
+      if (account && account.provider === "google") {
+        console.log(profile?.email);
+
+        // return profile && profile.email_verified && profile.email.endsWith("@example.com")
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
     },
     async redirect({ url, baseUrl }) {
       // if (url.startsWith(baseUrl + "/api/auth/callback/google")) {
